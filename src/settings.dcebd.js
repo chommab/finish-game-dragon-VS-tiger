b9bb041b46052cd68a1084a4d3ad391b6c58ed7a
chommab <136581572+chommab@users.noreply.github.com>
2023-08-06 10:31:41 +0800

    dragon vs tiger

---
+++ src/settings.dcebd.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scence/myScence.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"69b9a",main:"1cdb0"}};
